324f0c03344763beefc24b9937921c2d458371e3
delivey <deliveydev@gmail.com>
2021-03-13 14:19:14 +0200

    basic registering done

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,19 @@
-module.exports = function(app){
+module.exports = function (app) {
+  app.get("/", async function (req, res) {
+    req.session.username = "John"; // Uncomment this line to make the user register first
+    let username = req.session.username;
 
-    app.get('/', async function(req, res){
-        res.render('index', {foo: 'FOO'});
-    });
+    if (!username) res.render("index");
+    else res.redirect("/chat");
+  });
 
-}
+  app.get("/chat", async function (req, res) {
+    res.render("chat");
+  });
+
+  app.post("/register", async function (req, res) {
+    let username = req.body.username;
+    req.session.username = username;
+    res.redirect("/chat");
+  });
+};
--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
-const express = require('express')
-const ejs = require('ejs');
-const path = require('path')
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
 
-const app = express()
-const port = 8080
+const app = express();
+const port = 8080;
 
-app.use(express.static(path.join(__dirname, './../public')));
-app.set('views', path.join(__dirname, './../public/views'));
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "./../public")));
+app.set("views", path.join(__dirname, "./../public/views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
-require('./api/user.js')(app);
+app.use(
+  session({
+    secret: "keyboard cat",
+    maxAge: Date.now() + 30 * 86400 * 1000,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {},
+  })
+);
+
+require("./api/user.js")(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
